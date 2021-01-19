7811a0c8e4f65146973d88b71f73f19b53b8c7ce
shubhamvaishala <shubham.vaishala@thequint.com>
2021-01-19 15:26:53 +0530

    add params for filter data

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,6 +21,8 @@ var app = express(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/rssfeedsapi.js
+++ routes/rssfeedsapi.js
@@ -4,12 +4,15 @@ var axios = require('axios');
 var convert = require('xml-js');
 
 router.get('/googlerssfeeds', function(req, res, next){
+   var stype = req.param("section");
+   var slimit = req.param("limit");
+   console.log("section : "+ stype + " limit : "+ slimit);
    axios.get("https://www.thequint.com/stories.rss")
    .then((dt) => {
       const xml = dt.data;
       var jsonData = convert.xml2json(xml, {compact: true, spaces: 4});
       var rs = JSON.parse(jsonData);
-      res.render('index', {'data':  rs, 'lastBuildDate': dt});
+      res.render('index', {'xmldata': xml,'data':  rs, 'lastBuildDate': dt, 'type': stype, 'limit': slimit });
    })
 });
 module.exports = router;
